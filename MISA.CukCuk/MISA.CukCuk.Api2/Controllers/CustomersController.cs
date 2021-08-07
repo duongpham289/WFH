cb50fc836472256b91a82aa53077e2d81668ce68
duongpham289 <duongshin2000@gmail.com>
2021-08-07 21:54:45 +0700

    update connect to db

--- MISA.CukCuk/MISA.CukCuk.Api2/Controllers/CustomersController.cs
+++ MISA.CukCuk/MISA.CukCuk.Api2/Controllers/CustomersController.cs
@@ -5,6 +5,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using MISA.CukCuk.Api.Model;
+using System.Data;
+using MySqlConnector;
+using Dapper;
 
 namespace MISA.CukCuk.Api.Controllers
 {
@@ -15,9 +18,33 @@ namespace MISA.CukCuk.Api.Controllers
         // GET, POST, PUT, DELETE
 
         [HttpGet]
-        public List<Customer> GetCustomers()
+        public IActionResult GetCustomers()
         {
-            return null;
+
+            //Truy cap vào db:
+            //1. Khai bao thong tin ket noi db:
+            var connectionString = "Host = 47.241.69.179;" +
+                "Database = MF960_PHDUONG_CukCuk;" +
+                "User Id = dev;" +
+                "Password = 12345678;";
+
+            //2. Khoi tao doi tuong ket noi voi db
+            IDbConnection dbConnection = new MySqlConnection(connectionString);
+
+            //3. Lay du lieu:
+            var sqlCommand = "SELECT * FROM Customer";
+            var customers = dbConnection.Query<object>(sqlCommand);
+
+            //4. Tra ve cho client
+            var response = StatusCode(200, customers);
+            return response;
+        }
+
+        [HttpPost]
+        public List<Customer> InsertCustomers([FromBody] List<Customer> customers)
+        {
+            //var name = customers[0].FullName;
+            return customers;
         }
     }
 }
--- MISA.CukCuk/MISA.CukCuk.Api2/Startup.cs
+++ MISA.CukCuk/MISA.CukCuk.Api2/Startup.cs
@@ -28,10 +28,7 @@ namespace MISA.CukCuk.Api2
         {
 
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "MISA.CukCuk.Api2", Version = "v1" });
-            });
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
