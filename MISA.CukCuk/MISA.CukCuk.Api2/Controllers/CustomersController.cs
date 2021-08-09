85fa341a01b16a1e7021fe90264cffd0c91fb70b
duongpham289 <duongshin2000@gmail.com>
2021-08-09 17:38:08 +0700

    update thêm sửa xóa

--- MISA.CukCuk/MISA.CukCuk.Api2/Controllers/CustomersController.cs
+++ MISA.CukCuk/MISA.CukCuk.Api2/Controllers/CustomersController.cs
@@ -40,11 +40,83 @@ namespace MISA.CukCuk.Api.Controllers
             return response;
         }
 
-        [HttpPost]
-        public List<Customer> InsertCustomers([FromBody] List<Customer> customers)
+
+        [HttpGet("{customerId}")]
+        public IActionResult GetById(Guid customerId)
         {
-            //var name = customers[0].FullName;
-            return customers;
+            //Truy cap vào db:
+            //1. Khai bao thong tin ket noi db:
+            var connectionString = "Host = 47.241.69.179;" +
+                "Database = MF960_PHDUONG_CukCuk;" +
+                "User Id = dev;" +
+                "Password = 12345678;";
+
+            //2. Khoi tao doi tuong ket noi voi db
+            IDbConnection dbConnection = new MySqlConnection(connectionString);
+
+            //3. Lay du lieu:
+            var sqlCommand = $"SELECT * FROM Customer WHERE CustomerId = @CustomerIdParam";
+            DynamicParameters parameters = new DynamicParameters();
+            parameters.Add("@CustomerIdParam", customerId);
+            var customer = dbConnection.QueryFirstOrDefault<object>(sqlCommand,param:parameters);
+
+            //4. Tra ve cho client
+            var response = StatusCode(200, customer);
+            return response;
+        }
+
+        [HttpPost]
+        public IActionResult InsertCustomer(Customer customer)
+        {
+            //Truy cap vào db:
+            //1. Khai bao thong tin ket noi db:
+            var connectionString = "Host = 47.241.69.179;" +
+                "Database = MF960_PHDUONG_CukCuk;" +
+                "User Id = dev;" +
+                "Password = 12345678;";
+
+            //2. Khoi tao doi tuong ket noi voi db
+            IDbConnection dbConnection = new MySqlConnection(connectionString);
+            //Khai bao dynamicParam:
+            var dynamicParam = new DynamicParameters();
+
+
+            //3. Them du lieu vao db:
+            var columnsName = string.Empty;
+            var columnsParam = string.Empty;
+
+            //Doc tung prop cua obj:
+            var properties = customer.GetType().GetProperties();
+
+            //Duyet tung prop:
+            foreach (var prop in properties)
+            {
+                //lay ten cua prop
+                var propName = prop.Name;
+
+                //lay val cu prop
+                var propValue = prop.GetValue(customer);
+
+                //lay du lieu cua prop
+                var propType = prop.PropertyType;
+
+                //Them param tuong ung voi moi prop
+                dynamicParam.Add($"@{propName}", propValue);
+
+                columnsName += $"{propName},";
+                columnsParam += $"@{propName},";
+            }
+
+            columnsName = columnsName.Remove(columnsName.Length - 1, 1);
+            columnsParam = columnsParam.Remove(columnsParam.Length - 1, 1);
+
+            var sqlCommand = $"INSERT INTO Customer({columnsName}) VALUE({columnsParam})";
+
+            var rowEfects = dbConnection.Execute(sqlCommand, param:dynamicParam);
+
+            //4. Tra ve cho client
+            var response = StatusCode(200, rowEfects);
+            return response;
         }
     }
 }
--- MISA.CukCuk/MISA.CukCuk.Api2/Startup.cs
+++ MISA.CukCuk/MISA.CukCuk.Api2/Startup.cs
@@ -28,7 +28,13 @@ namespace MISA.CukCuk.Api2
         {
 
             services.AddControllers();
+
             services.AddSwaggerGen();
+
+            services.AddControllers().AddJsonOptions(jsonOptions =>
+            {
+                jsonOptions.JsonSerializerOptions.PropertyNamingPolicy = null;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
